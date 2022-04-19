0afb2bb9b07172f0f6ca9def7ab155278c5dbfb5
Alexander Tih <dgioto@gmail.com>
2022-04-19 21:57:01 +0300

    fix

--- app/src/main/java/com/dgioto/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/dgioto/photogallery/PhotoGalleryFragment.kt
@@ -6,17 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.dgioto.photogallery.api.FlickrApi
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 
 private const val TAG = "PhotoGalleryFragment"
 
