b06933db9997bb4f8bd80a3933cce3ac115f5e59
Alexander Tih <dgioto@gmail.com>
2022-05-06 15:25:38 +0300

    preparing to download via URL

--- app/src/main/java/com/dgioto/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/dgioto/photogallery/api/FlickrApi.kt
@@ -1,7 +1,9 @@
 package com.dgioto.photogallery.api
 
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface FlickrApi {
 
@@ -12,4 +14,8 @@ interface FlickrApi {
             "&nojsoncallback=1" +
             "&extras=url_s")
     fun fetchPhotos(): Call<FlickResponse>
+
+    @GET
+    fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
+
 }
