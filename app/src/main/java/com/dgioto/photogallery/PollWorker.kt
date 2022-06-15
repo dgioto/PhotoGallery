90527a302486d7c94a5722d28189dee7b8d8e7e8
Alexander Tih <dgioto@gmail.com>
2022-06-15 15:35:31 +0300

    Sending Broadcast Intents

--- app/src/main/java/com/dgioto/photogallery/PollWorker.kt
+++ app/src/main/java/com/dgioto/photogallery/PollWorker.kt
@@ -2,6 +2,7 @@ package com.dgioto.photogallery
 
 import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -58,9 +59,16 @@ class PollWorker(private val context: Context, workerParams: WorkerParameters)
                 .build()
             val notificationManager = NotificationManagerCompat.from(context)
             notificationManager.notify(0, notification)
+
+            //Отправка широковещательного интента
+            context.sendBroadcast(Intent(ACTION_SHOW_NOTIFICATION))
         }
 
         return Result.success()
     }
 
+    companion object {
+        const val ACTION_SHOW_NOTIFICATION =
+            "com.bignerdranch.android.photogallery.SHOW_NOTIFICATION"
+    }
 }
