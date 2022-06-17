1ad347df2a21b7dbc166e3980d7531d20b4c3485
Alexander Tih <dgioto@gmail.com>
2022-06-17 15:40:31 +0300

    Creating and Registering a Dynamic Receiver

--- app/src/main/java/com/dgioto/photogallery/NotificationReceiver.kt
+++ app/src/main/java/com/dgioto/photogallery/NotificationReceiver.kt
@@ -1,9 +1,12 @@
 package com.dgioto.photogallery
 
+import android.app.Activity
+import android.app.Notification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.core.app.NotificationManagerCompat
 
 private const val TAG = "NotificationReceiver"
 
@@ -11,7 +14,16 @@ private const val TAG = "NotificationReceiver"
 class NotificationReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
+        //Реализация приемника результатов
+        Log.i(TAG, "received result: $resultCode")
+        if (resultCode != Activity.RESULT_OK) {
+            // Активность переднего плана отменила возврат трансляции.
+        }
 
-        Log.i(TAG, "received broadcast: ${intent.action}")
+        val requestCode = intent.getIntExtra(PollWorker.REQUEST_CODE, 0)
+        val notification: Notification = intent.getParcelableExtra(PollWorker.NOTIFICATION)!!
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.notify(requestCode, notification)
     }
 }
--- app/src/main/java/com/dgioto/photogallery/PollWorker.kt
+++ app/src/main/java/com/dgioto/photogallery/PollWorker.kt
@@ -1,5 +1,6 @@
 package com.dgioto.photogallery
 
+import android.app.Notification
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
@@ -57,19 +58,27 @@ class PollWorker(private val context: Context, workerParams: WorkerParameters)
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
                 .build()
-            val notificationManager = NotificationManagerCompat.from(context)
-            notificationManager.notify(0, notification)
 
-            //Отправка широковещательного интента
-            //Отправка с разрешением
-            context.sendBroadcast(Intent(ACTION_SHOW_NOTIFICATION), PERM_PRIVATE)
+            showBackgroundNotification(0, notification)
         }
-
         return Result.success()
+    }
+
+    //Отправка упорядоченных широковещательных интентов
+    private fun showBackgroundNotification(
+        requestCode: Int, notification: Notification
+    ) {
+        val intent = Intent(ACTION_SHOW_NOTIFICATION).apply {
+            putExtra(REQUEST_CODE, requestCode)
+            putExtra(NOTIFICATION, notification)
+        }
+        context.sendOrderedBroadcast(intent, PERM_PRIVATE)
     }
 
     companion object {
         const val ACTION_SHOW_NOTIFICATION = "com.dgioto.photogallery.SHOW_NOTIFICATION"
         const val PERM_PRIVATE = "com.dgioto.photogallery.PRIVATE"
+        const val REQUEST_CODE = "REQUEST_CODE"
+        const val NOTIFICATION = "NOTIFICATION"
     }
 }
